76198a16540ffe4dbb4eb464099fd0e481062bca
kakekita <117062289+kakekita@users.noreply.github.com>
2023-12-08 07:59:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -13,8 +13,17 @@ var students_list = null;
 
 let double_click_count1 = 0;
 let double_click_count2 = 0;
+let double_click_count3 = 0;
 
 var d = {};
+
+var add_elem_id = "";
+var start_date = "2023/11/26";
+
+var timeoutIDs = {
+  s: null,
+  p: null,
+};
 
 req.onreadystatechange = function () {
   if (req.readyState == 4) {
@@ -52,15 +61,24 @@ req.onreadystatechange = function () {
         elem.setAttribute("id", "s_li_" + c);
         elem.addEventListener("click", s_li1_click, false);
         //elem.addEventListener("touchstart", s_li1_click, false);
-        elem.textContent = i["title"] + "  |  " + i["authors"];
+        elem.textContent = i["title"] + " | " + i["authors"];
         d["s_list"].appendChild(elem);
         getElements();
+        setStatus("s", "検索結果を更新しました");
       }
     }
   } else {
     console.log("通信中...");
   }
 };
+
+function setStatus(id, str) {
+  d[id + "_status"].innerHTML = str;
+  clearTimeout(timeoutIDs[id]);
+  timeoutIDs[id] = setTimeout(function () {
+    d[id + "_status"].innerHTML = "　";
+  }, 3000);
+}
 
 function s_li1_click(e) {
   if (!double_click_count1) {
@@ -102,7 +120,6 @@ function s_li1_click(e) {
           "name",
           useri["id"]
         );
-      getDataList();
     });
   }
 }
@@ -124,9 +141,47 @@ function s_li2_click(e) {
     // 回数をリセット
     double_click_count2 = 0;
     var id = "book" + e.target.id.replace("s_li2_", "");
-    alert("削除しました。");
     addData(id, bookList[id][0], bookList[id][1], false, "name", useri.id);
-    getDataList();
+  }
+}
+
+function p_data_click(e) {
+  if (!double_click_count3) {
+    // タップの回数を+1
+    double_click_count3++;
+    // 500ミリ秒以内に2回目のタップがされればダブルタップと判定
+    setTimeout(function () {
+      double_click_count3 = 0;
+    }, 500);
+
+    // ダブルタップ
+  } else {
+    // 拡大をさせない
+    e.preventDefault();
+    // 処理を記述
+    // 回数をリセット
+    double_click_count3 = 0;
+
+    var ind = e.currentTarget.getAttribute("name").split("_")[3];
+    var id = e.currentTarget
+      .getAttribute("name")
+      .replace("page", "data")
+      .replace(/_[0-9]*$/, "");
+
+    var db = firebase.firestore();
+    var userRef = db.collection("page").doc(useri.id);
+    var k = "list";
+    userRef.get().then((e) => {
+      var d2 = e.data();
+      if (d2) {
+        if (!Object.keys(d2[k]).includes(id)) d2[k][id] = [];
+        d2[k][id].splice(ind, 1);
+        userRef.set(d2, { merge: true }).then((e) => {
+          getDataList();
+          setStatus("p", "カレンダーを更新しました");
+        });
+      }
+    });
   }
 }
 
@@ -142,6 +197,38 @@ function s_input_click(e) {
   }
 
   search(elem.value.split(/\s+/));
+}
+
+function p_input_click(e) {
+  d["modal-001__close"].checked = true;
+  if (d["p_book_selects"].value.length < 1) {
+    return false;
+  }
+  if (d["p_page_input"] <= 0) {
+    return false;
+  }
+  var str = add_elem_id.split("_");
+  str[0] = "2023";
+  var t = String(parseInt(str[1]) * 7 + parseInt(str[2]));
+
+  var datas = {
+    date: new Date(new Date(start_date).getTime() + t * 86400000).toLocaleDateString("ja-JP"),
+    id: parseInt(
+      Object.keys(bookList)
+        .filter((k) => bookList[k][0] + " | " + bookList[k][1] === d["p_book_selects"].value)[0]
+        .replace("book", "")
+    ),
+    count: parseInt(d["p_page_input"].value),
+  };
+  var se_d = {
+    s: new Date(start_date),
+    e: new Date(datas["date"]),
+  };
+  console.log(se_d);
+  var days = (se_d["e"] - se_d["s"]) / 86400000;
+  var key = "data_" + Math.floor(days / 7) + "_" + (days % 7);
+
+  addData3(key, datas, "page", useri.id);
 }
 
 function d_input_click(e) {
@@ -250,7 +337,10 @@ function addData(num, s, s2, s3, c, d) {
   var db = firebase.firestore();
   var userRef = db.collection(c).doc(d);
   num = String(num);
-  userRef.set({ [num]: [s, s2, s3] }, { merge: true });
+  userRef.set({ [num]: [s, s2, s3] }, { merge: true }).then((e) => {
+    getDataList();
+    setStatus("s", "ブックマークを更新しました");
+  });
 }
 
 function addData2(num, s, c, d) {
@@ -258,6 +348,24 @@ function addData2(num, s, c, d) {
   var userRef = db.collection(c).doc(d);
   num = String(num);
   userRef.set({ [num]: s }, { merge: true });
+}
+
+function addData3(num, s, c, d) {
+  var db = firebase.firestore();
+  var userRef = db.collection(c).doc(d);
+  var k = "list";
+  userRef.get().then((e) => {
+    var d2 = e.data();
+    if (d2) {
+      if (!Object.keys(d2[k]).includes(num)) d2[k][num] = [];
+      d2[k][num].push(s);
+      console.log(d2);
+      userRef.set(d2, { merge: true }).then((e) => {
+        getDataList();
+        setStatus("p", "カレンダーを更新しました");
+      });
+    }
+  });
 }
 
 function getDataList() {
@@ -275,7 +383,7 @@ function getDataList() {
         elem.setAttribute("id", "s_li2_" + v.replace("book", ""));
         elem.addEventListener("click", s_li2_click, false);
         //elem.addEventListener("touchstart", s_li2_click, false);
-        elem.textContent = bookList[v][0] + "  |  " + bookList[v][1];
+        elem.textContent = bookList[v][0] + " | " + bookList[v][1];
         //d["s_list2"].appendChild(elem);
         if (bookList[v][2]) {
           d["s_list2"].appendChild(elem);
@@ -289,7 +397,75 @@ function getDataList() {
   var postRef2 = db2.collection("page").doc(useri.id);
   postRef2.get().then((e) => {
     var d2_2 = e.data();
+
     if (d2_2) {
+      while (d["p_table_view"].firstChild) {
+        d["p_table_view"].removeChild(d["p_table_view"].firstChild);
+      }
+      d["p_table_view"].insertAdjacentHTML(
+        "beforeend",
+        '<tr style="height: 20px; background-color: khaki"><th>日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th>土</th></tr>'
+      );
+      var v = "list";
+      var keys = Object.keys(d2_2[v]);
+      var maxLen = { x: 7, y: 50 };
+      var date1 = new Date(start_date);
+      for (var dy = 0; dy < maxLen.y; dy++) {
+        var elem1 = document.createElement("tr");
+        elem1.setAttribute("id", "data_" + dy);
+        d["p_table_view"].appendChild(elem1);
+        getElements();
+        for (var dx = 0; dx < maxLen.x; dx++) {
+          var elem2 = document.createElement("td");
+          elem2.setAttribute("id", "data_" + dy + "_" + dx);
+          d["data_" + dy].appendChild(elem2);
+          getElements();
+          var elem3 = document.createElement("span");
+          elem3.innerHTML = date1.toLocaleDateString("ja-JP");
+          elem3.style.fontWeight = "bold";
+          date1.setDate(date1.getDate() + 1);
+          d["data_" + dy + "_" + dx].appendChild(elem3);
+          var elem4 = document.createElement("ul");
+          elem4.setAttribute("id", "ul_" + dy + "_" + dx);
+          d["data_" + dy + "_" + dx].appendChild(elem4);
+          getElements();
+          if (keys.includes("data_" + dy + "_" + dx)) {
+            for (var a = 0; a < d2_2[v]["data_" + dy + "_" + dx].length; a++) {
+              var elem2_2 = document.createElement("li");
+              var b_stat = d2_2[v]["data_" + dy + "_" + dx][a];
+              var bs_stat = bookList["book" + b_stat["id"]];
+              elem2_2.insertAdjacentHTML(
+                "beforeend",
+                "<span>" +
+                  bs_stat[0] +
+                  "</span><br><span>" +
+                  bs_stat[1] +
+                  "</span><br><span>" +
+                  b_stat["count"] +
+                  "ページまで</span><br>"
+              );
+              elem2_2.setAttribute("id", "page_" + dy + "_" + dx + "_" + a);
+              elem2_2.setAttribute("name", "page_" + dy + "_" + dx + "_" + a);
+              elem2_2.addEventListener("click", p_data_click, false);
+              d["ul_" + dy + "_" + dx].appendChild(elem2_2);
+              getElements();
+              for (var s in d["page_" + dy + "_" + dx + "_" + a].children) {
+                if (s % 2 == 0) resize("page_" + dy + "_" + dx + "_" + a, s);
+              }
+            }
+          }
+          var elem2_3 = document.createElement("a");
+          elem2_3.setAttribute("href", "javascript:void(0)");
+          elem2_3.setAttribute("onclick", "openPageDialog(this)");
+          elem2_3.setAttribute("id", "a_" + dy + "_" + dx);
+          elem2_3.textContent = "＋";
+          elem2_3.style.color = "orange";
+          elem2_3.style.textDecoration = "none";
+          d["data_" + dy + "_" + dx].appendChild(elem2_3);
+          getElements();
+        }
+      }
+      /*
       for (var v in d2_2) {
         var keys = Object.keys(d2_2[v]);
         var row_count = 0;
@@ -305,33 +481,53 @@ function getDataList() {
           var elem2 = document.createElement("td");
           elem2.setAttribute("id", keys[i]);
           var book_stat = bookList["book" + d2_2[v][keys[i]][1]];
-          elem2.insertAdjacentHTML(
-            "beforeend",
-            "<div name='in_" +
-              keys[i] +
-              "'><p>" +
-              d2_2[v][keys[i]][0] +
-              "</p><p>" +
-              book_stat[0] +
-              " | " +
-              book_stat[1] +
-              "</p><p>" +
-              d2_2[v][keys[i]][2] +
-              "</p></div>"
-          );
+          var elem2_2 = document.createElement("button");
+          elem2_2.addEventListener("click", openPageDialog, false);
 
           d["data_" + ind.y].appendChild(elem2);
         }
       }
+      */
+    } else {
+      var data = { list: {} };
+      postRef2.set(data, { merge: true }).then((e) => {
+        location.reload();
+      });
     }
   });
 }
 
+function resize(id, s) {
+  d[id].children[s].removeAttribute("style");
+  console.log(
+    d[id].children[s].getBoundingClientRect().height,
+    d[id].children[0].getBoundingClientRect().height * 2
+  );
+
+  for (
+    let size = 60;
+    d[id].children[s].getBoundingClientRect().height >
+    d[id].children[0].getBoundingClientRect().height * 2;
+    size -= 2
+  ) {
+    d[id].children[s].style.fontSize = size + "px";
+  }
+}
+
+function openPageDialog(el) {
+  add_elem_id = el.getAttribute("id");
+  d["modal-001__open"].checked = true;
+}
+
 function setPageSelects(bl) {
+  while (d["p_book_selects"].firstChild) {
+    d["p_book_selects"].removeChild(d["p_book_selects"].firstChild);
+  }
+  d["p_page_input"].value = 0;
   for (var k of Object.keys(bl)) {
     if (bl[k][2]) {
       var elem = document.createElement("option");
-      elem.textContent = bl[k][0] + "  |  " + bl[k][1];
+      elem.textContent = bl[k][0] + " | " + bl[k][1];
       d["p_book_selects"].appendChild(elem);
     }
   }
@@ -339,7 +535,9 @@ function setPageSelects(bl) {
 
 function getCSV() {
   var req = new XMLHttpRequest();
-  req.open("get", "./students/list.csv", true);
+  req.open("get", "list.csv", true);
+  //req.open("get", "./list.csv", true);
+  req.setRequestHeader("content-type", "application/x-www-form-urlencoded;charset=UTF-8");
   req.send(null);
 
   req.onload = function () {
@@ -421,6 +619,7 @@ function setup(i) {
   d["tab_parent"].style.visibility = "visible";
 
   d["s_button"].addEventListener("click", s_input_click, false);
+  d["p_button"].addEventListener("click", p_input_click, false);
   //d["s_more_button"].addEventListener("click", s_input_click, false);
   getDataList();
 }
